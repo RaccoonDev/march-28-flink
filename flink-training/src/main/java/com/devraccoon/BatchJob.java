188ae91ead434f587cfcba7e0b3072d0868c676b
Dmytro Mykhailov <dmytro.m@miro.com>
2022-03-28 12:30:04 +0200

    Work count example

--- flink-training/src/main/java/com/devraccoon/BatchJob.java
+++ flink-training/src/main/java/com/devraccoon/BatchJob.java
@@ -18,49 +18,41 @@
 
 package com.devraccoon;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
 
-/**
- * Skeleton for a Flink Batch Job.
- *
- * <p>For a tutorial how to write a Flink batch application, check the
- * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution,
- * change the main class in the POM.xml file to this class (simply search for 'mainClass')
- * and run 'mvn clean package' on the command line.
- */
 public class BatchJob {
 
 	public static void main(String[] args) throws Exception {
-		// set up the batch execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
+
+		DataSource<String> stringDataSource = env.readTextFile("data/words.txt");
 		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataSet<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * https://flink.apache.org/docs/latest/apis/batch/index.html
-		 *
-		 * and the examples
-		 *
-		 * https://flink.apache.org/docs/latest/apis/batch/examples.html
-		 *
+			element1 > sam said something
+			element2 > something else happened
+		 */
+		stringDataSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+			@Override
+			public void flatMap(String element, Collector<Tuple2<String, Integer>> collector) throws Exception {
+				String[] words = element.split(" ");
+				for(String word: words) {
+					collector.collect(Tuple2.of(word, 1));
+				}
+			}
+		}).groupBy(0).sum(1).print();
+		/*
+			element1 > (sam, 1)
+			element2 > (said, 1)
+			element3 > (something, 1)
+			element4 > (something, 1)
+			element5 > (else, 1)
+			...
 		 */
 
-		// execute program
 		env.execute("Flink Batch Java API Skeleton");
 	}
 }
--- flink-training/src/main/java/com/devraccoon/StreamingJob.java
+++ flink-training/src/main/java/com/devraccoon/StreamingJob.java
@@ -18,45 +18,37 @@
 
 package com.devraccoon;
 
+import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
-/**
- * Skeleton for a Flink Streaming Job.
- *
- * <p>For a tutorial how to write a Flink streaming application, check the
- * tutorials and examples on the <a href="https://flink.apache.org/docs/stable/">Flink Website</a>.
- *
- * <p>To package your application into a JAR file for execution, run
- * 'mvn clean package' on the command line.
- *
- * <p>If you change the name of the main class (with the public static void main(String[] args))
- * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
- */
 public class StreamingJob {
 
 	public static void main(String[] args) throws Exception {
 		// set up the streaming execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * https://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
+		DataStream<String> streamOfLines = env.readTextFile("data/words.txt");
+		SingleOutputStreamOperator<Tuple2<String, Integer>> streamOfPairs = streamOfLines.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+			@Override
+			public void flatMap(String element, Collector<Tuple2<String, Integer>> collector) throws Exception {
+				String[] words = element.split(" ");
+				for (String word : words) {
+					collector.collect(Tuple2.of(word, 1));
+				}
+			}
+		});
+
+		streamOfPairs
+				.keyBy(t -> t.f0)
+				.sum(1)
+				.writeAsText("out/wordsCount.txt")
+				.setParallelism(1);
 
 		// execute program
 		env.execute("Flink Streaming Java API Skeleton");
