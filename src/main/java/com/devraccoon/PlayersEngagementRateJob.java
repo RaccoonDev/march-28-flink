e37ba0d55d7e4655b77b8e08802b2a96d1b2ab7d
Kamran Alipour <kamran.alipour@bisnode.com>
2022-03-30 11:11:57 +0200

    Initial commit

--- src/main/java/com/devraccoon/PlayersEngagementRateJob.java
+++ src/main/java/com/devraccoon/PlayersEngagementRateJob.java
@@ -7,23 +7,25 @@ import com.devraccoon.processes.PlayerEventToCounts;
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.connector.file.sink.FileSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -48,14 +50,27 @@ public class PlayersEngagementRateJob {
         final String outputPath = params.get("output-path") != null ? params.get("output-path") : "file://" + pwd + "/out/";
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        env.setRestartStrategy(
+                RestartStrategies.fixedDelayRestart(
+                        5, Duration.ofMinutes(1).toMillis()
+                )
+        );
+
         env.enableCheckpointing(Duration.ofSeconds(10).toMillis());
+        env.getCheckpointConfig().setCheckpointTimeout(Duration.ofMinutes(15).toMillis());
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        env.getCheckpointConfig().setTolerableCheckpointFailureNumber(3);
         env.getCheckpointConfig().setCheckpointStorage(outputPath + "/checkpoints");
+
+        env.getCheckpointConfig().setExternalizedCheckpointCleanup(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION
+        );
 
         KafkaSource<PlayerEvent> kafkaSource = KafkaSource.<PlayerEvent>builder()
                 .setBootstrapServers(bootstrapServers)
                 .setTopics(topic)
-                .setGroupId("battle-net-events-processor-group-1")
-                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setGroupId("battle-net-events-processor-group-2-cluster")
+                .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
                 .setDeserializer(new PlayerEventAvroDeserializerScheme(schemaRegistryUrl, topic))
                 .build();
 
@@ -69,19 +84,28 @@ public class PlayersEngagementRateJob {
                 })
                 .withIdleness(Duration.ofSeconds(2));
 
-        DataStream<PlayerEvent> playerEvents = env.fromSource(kafkaSource, watermarkStrategy, "player-events");
+        DataStream<PlayerEvent> playerEvents = env.fromSource(
+                kafkaSource,
+                watermarkStrategy,
+                "player-events");
+
+        processCountOffline(playerEvents, outputPath);
+        processPlayersEngagement(playerEvents, outputPath);
+
+        env.execute("Battle Net Engagement Rate Job");
+    }
+
+    private static void processCountOffline(DataStream<PlayerEvent> playerEvents, String outputPath) {
 
         KeyedStream<PlayerEvent, UUID> playerEventUUIDKeyedStream = playerEvents.keyBy(PlayerEvent::getPlayerId);
-
         WindowedStream<PlayerEvent, UUID, TimeWindow> window = playerEventUUIDKeyedStream.window(SlidingEventTimeWindows.of(Time.seconds(20), Time.seconds(5)));
-
         DataStream<Integer> process = window.process(new DetectOfflineEvent());
 
         DataStream<String> countOfUsersWithoutOfflineEvents = process
                 .windowAll(SlidingEventTimeWindows.of(Time.seconds(20), Time.seconds(5)))
                 .apply(new AllWindowFunction<Integer, String, TimeWindow>() {
                     @Override
-                    public void apply(TimeWindow timeWindow, Iterable<Integer> iterable, Collector<String> collector) {
+                    public void apply(TimeWindow timeWindow, Iterable<Integer> iterable, Collector<String> collector) throws Exception {
                         long count = StreamSupport.stream(iterable.spliterator(), false).count();
                         String message = String.format("Window: [%s]; Number of people without offline event: %d", timeWindow.toString(), count);
                         collector.collect(message);
@@ -94,24 +118,34 @@ public class PlayersEngagementRateJob {
         ).build();
 
         countOfUsersWithoutOfflineEvents.sinkTo(fileSink);
-
-        processPlayersEngagement(playerEvents, outputPath);
-
-        env.execute("Battle Net Engagement Rate Job");
     }
 
     private static void processPlayersEngagement(DataStream<PlayerEvent> playerEvents, String outputPath) {
 
-        DataStream<Tuple2<Long, Long>> onlineAndRegistrationCountsStream = playerEvents.process(new PlayerEventToCounts());
+        DataStream<Tuple2<Long, Long>> onlineAndRegistrationCountsStream = playerEvents
+                .keyBy(k -> 0)
+                .process(new PlayerEventToCounts())
+                .uid("player-events-processor")
+                .name("");
 
-        DataStream<String> playersEngagementRatesStream = onlineAndRegistrationCountsStream.map((MapFunction<Tuple2<Long, Long>, String>) pair -> {
-            Long registrations = pair.f0;
-            Long online = pair.f1;
-            return String.format(
-                    "Number of registered players: %d; Online players: %d; Rate of online users: %.2f",
-                    registrations, online, ((double) online / (double) registrations) * 100.0);
+        DataStream<String> playersEngagementRatesStream = onlineAndRegistrationCountsStream.map(new MapFunction<Tuple2<Long, Long>, String>() {
+            @Override
+            public String map(Tuple2<Long, Long> pair) throws Exception {
+                Long registrations = pair.f0;
+                Long online = pair.f1;
+                return String.format(
+                        "Number of registered players: %d; Online players: %d; Rate of online users: %.2f",
+                        registrations, online, ((double) online / (double) registrations) * 100.0);
+            }
         });
 
-        playersEngagementRatesStream.writeAsText(outputPath + "/engagementRates.txt", FileSystem.WriteMode.OVERWRITE);
+        FileSink<String> fileSink = FileSink.forRowFormat(
+                        new Path(outputPath + "/engagementRate"),
+                        new SimpleStringEncoder<String>("UTF-8")
+                )
+                .build();
+
+        playersEngagementRatesStream
+                .sinkTo(fileSink);
     }
 }
